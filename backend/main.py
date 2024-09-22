79fa45a480e5fa4ea7d495013a3ee2b7db75bc32
DeenankSharma <sharmadeenank@gmail.com>
2024-09-22 15:02:10 +0530

    initial uplaod

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for,jsonify
 from flask_cors import CORS
 from routes import configure_routes
 
-app = Flask(__name__)
-CORS(app)
+b27app = Flask(__name__)
+CORS(b27app)
 
-configure_routes(app)
+configure_routes(b27app)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    b27app.run(debug=True)
