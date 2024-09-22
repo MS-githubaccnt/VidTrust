043e84038955a37e5d0b28f067f97ae4d33e42e1
DeenankSharma <sharmadeenank@gmail.com>
2024-09-23 00:08:10 +0530

    feat : google sign in basic ui

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,71 @@
-from flask import Flask, render_template, request, redirect, url_for,jsonify
+from functools import wraps
+from flask import Flask, request, redirect, jsonify, make_response
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+import requests
 from routes import configure_routes
 
-b27app = Flask(__name__)
-CORS(b27app)
+import jwt
+from urllib.parse import urlencode
+import json
 
-configure_routes(b27app)
+load_dotenv()  # This loads the variables from .env file
+
+app = Flask(__name__)
+
+CORS(app, origins=[os.getenv('CLIENT_URL')], supports_credentials=True)
+
+config = {
+    'client_id': os.getenv('GOOGLE_CLIENT_ID'),
+    'client_secret': os.getenv('GOOGLE_CLIENT_SECRET'),
+    'auth_url': 'https://accounts.google.com/o/oauth2/v2/auth',
+    'token_url': 'https://oauth2.googleapis.com/token',
+    'redirect_url': os.getenv('REDIRECT_URL'),
+    'client_url': os.getenv('CLIENT_URL'),
+    'token_secret': os.getenv('TOKEN_SECRET'),
+    'token_expiration': 36000,
+    'post_url': 'https://jsonplaceholder.typicode.com/posts',
+}
+
+def get_auth_params():
+    return urlencode({
+        'client_id': config['client_id'],
+        'redirect_uri': config['redirect_url'],
+        'response_type': 'code',
+        'scope': 'openid profile email',
+        'access_type': 'offline',
+        'state': 'standard_oauth',
+        'prompt': 'consent',
+    })
+
+def get_token_params(code):
+    return urlencode({
+        'client_id': config['client_id'],
+        'client_secret': config['client_secret'],
+        'code': code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': config['redirect_url'],
+    })
+
+
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.cookies.get('token')
+        if not token:
+            return jsonify({'message': 'Unauthorized'}), 401
+        try:
+            jwt.decode(token, config['token_secret'], algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Invalid token'}), 401
+        return f(*args, **kwargs)
+    return decorated
+
+configure_routes(app)
 
 
 if __name__ == '__main__':
-    b27app.run(debug=True)
+    app.run(debug=True)
--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,7 @@
-from controllers import login_task,register_task
+from controllers.auth_controllers import get_auth_url,auth_token, check_logged_in, logged_out
 
 def configure_routes(app):
-    app.route('/login', methods=['POST'])(login_task)
-    app.route('/register', methods=['POST'])(register_task)
+    app.route('/auth/url', methods=['GET'])(get_auth_url)
+    app.route('/auth/token', methods=['GET'])(auth_token)
+    app.route('/auth/logged_in', methods=['GET'])(check_logged_in)
+    app.route('/auth/logout', methods=['POST'])(logged_out)
