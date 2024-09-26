6a36b5b893d15b1c10e2e371242ce6d76e4729dd
Tanisha Dixit <dixittanisha02@gmail.com>
2024-09-26 16:26:17 +0530

    conf: adds routes for signing data and verifying signatures

--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,7 @@
 from backend.controllers.user_controllers import login_task,register_task
-
+from backend.controllers.signature_controllers import sign_combined_data,verify_signature
 def configure_routes(app):
     app.route('/login', methods=['POST'])(login_task)
     app.route('/register', methods=['POST'])(register_task)
+    app.route('/sign',methods=['POST'])(sign_combined_data)
+    app.route('/verify',methods=['POST'])(verify_signature)
