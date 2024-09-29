2ad8181db3b193ade7a87ac7b79909e979ff12b4
Tanisha Dixit <dixittanisha02@gmail.com>
2024-09-29 14:30:27 +0530

    feat: integrates buckets to implement setting and extraction of metadata from video files, adds compression and resizing of frames data before storage and modifies .gitignore

--- backend/controllers/signature_controllers.py
+++ backend/controllers/signature_controllers.py
@@ -1,26 +1,39 @@
 from cryptography.hazmat.primitives.asymmetric import rsa,padding
 from cryptography.hazmat.primitives import serialization,hashes
 from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.backends import default_backend
 import cv2
 import json
 import base64
 import ffmpeg
 import os
+import sys
+import requests
+from supabase import create_client,Client
+import subprocess
 from dotenv import load_dotenv
+import zlib
 load_dotenv()
 
+supabase_url = os.environ['SUPABASE_URL']
+supabase_anon_key = os.environ['SUPABASE_ANON_KEY']
+client:Client = create_client(supabase_url, supabase_anon_key)
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+print(base_dir)
+sys.path.append(base_dir)
+from backend.firestore_connection.firestore import connect_signature_database
 
 def generate_key():
-    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-
+    private_key = rsa.generate_private_key(
+        public_exponent=65537, 
+        key_size=2048
+        )
     private_pem = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.TraditionalOpenSSL,
         encryption_algorithm=serialization.NoEncryption()
-    )
-
-    with open("private_key.pem", "wb") as f:
-        f.write(private_pem)
+        )
 
     public_key = private_key.public_key()
     public_pem = public_key.public_bytes(
@@ -28,10 +41,8 @@ def generate_key():
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     )
 
-    with open("public_key.pem", "wb") as f:
-        f.write(public_pem)
-
-    print("Public and Private keys generated successfully.")
+    print("Public and Private keys generated successfully.", private_pem,public_pem)
+    return private_pem,public_pem
 
 def frame_capture(path):
     cap=cv2.VideoCapture(path)
@@ -53,24 +64,75 @@ def frame_capture(path):
     return frames
 
 def encode_frame_to_base64(frame):
-    _,buffer=cv2.imencode(".jpg",frame)
-    return base64.b64encode(buffer).decode('utf-8')
+    frame_resized = cv2.resize(frame, (640, 360), interpolation=cv2.INTER_AREA)
+    is_success, buffer = cv2.imencode(".jpg", frame_resized, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
+    frame_base64 = base64.b64encode(buffer).decode('utf-8')
+    return frame_base64
 
 def extract_metadata(video_path):
-    probe=ffmpeg.probe(video_path, cmd=r'C:/ffmpeg/bin/ffprobe.exe')
+    probe=ffmpeg.probe(video_path)
     video_stream=next((stream for stream in probe['streams'] if stream['codec_type']=='video'),None)
     print(video_stream)
     return video_stream
 
+def extract_signature_and_public_key():
+    ffmpeg_command=['ffprobe','-v','quiet','-print_format','json','-show_format' ,"output_video.mkv"]
+    result = subprocess.run(ffmpeg_command,capture_output=True,text=True)
+    metadata=json.loads(result.stdout)
+    
+    tags = metadata['format']['tags']["['SIGNATURE"]
+    values=tags.split("', 'public_key=")
+
+    signature_b64=values[0]
+    public_key_b64=values[1].rstrip("']")
+
+    print(signature_b64)
+    print(public_key_b64)
+
+    signature = base64.b64decode(signature_b64)
+    public_key = base64.b64decode(public_key_b64)
+
+    print("Signature:", signature)
+    print("Public Key:", public_key)
+
+    return public_key,signature
+
+
+def set_signature_and_public_key(local_video_path, signature, public_pem):
+    signature_b64 = base64.b64encode(signature).decode('utf-8')
+    public_pem_b64 = base64.b64encode(public_pem).decode('utf-8')
+    print(signature_b64,"\n\n",public_pem_b64)
+
+    metadata_args = {
+        'metadata': [
+            f'signature={signature_b64}',
+            f'public_key={public_pem_b64}',
+        ]
+    }
+    print(metadata_args)
+    input_stream=ffmpeg.input("test_video.mkv")
+    output_stream=input_stream.output('output_video.mkv',vcodec='copy',acodec='copy', **metadata_args)
+    output_stream.run(overwrite_output=True)
+
+def upload_video_to_supabase(local_file_path, remote_file_name):
+    with open(local_file_path, "rb") as file:
+        client.storage.from_("video").upload(remote_file_name, file)
+
+def load_public_key(pem_data):
+    return serialization.load_pem_public_key(
+        pem_data, 
+        backend=default_backend()
+    )
 def combine_data(user_data,metadata,frames):
     encoded_frames=[encode_frame_to_base64(frame) for frame in frames]
+    compressed_frames = zlib.compress(''.join(encoded_frames).encode('utf-8'))
 
     combined_data={
         "user_data":user_data,
         "metadata":metadata,
-        "frames":encoded_frames
+        "frames":base64.b64encode(compressed_frames).decode('utf-8')
     } 
-
+    
     combined_data_str=json.dumps(combined_data)
     return combined_data_str
 
@@ -88,13 +150,12 @@ def sign_combined_data(user_data,metadata,frames):
         hashes.SHA256()
     )
 
-    with open("signature.bin","wb") as f:
-        f.write(signature)
+    print(signature)
+    return signature,combined_data
 
-def verify_signature(user_data,metadata,frames,signature):
-    with open("public_key.pem", "rb") as f:
-        public_key = serialization.load_pem_public_key(f.read())
-        combined_data=combine_data(user_data,metadata,frames)
+def verify_signature(user_data,metadata,frames,signature,public_pem):
+    public_key = load_public_key(public_pem)  
+    combined_data=combine_data(user_data,metadata,frames)
     try:
         public_key.verify(
             signature,
@@ -109,15 +170,35 @@ def verify_signature(user_data,metadata,frames,signature):
     except InvalidSignature:
         print("The video has been tampered with or is not authentic.")
 
+def download_video(url, local_path):
+    response = requests.get(url)
+    with open(local_path, 'wb') as file:
+        file.write(response.content)
+
+video_url = os.environ["SUPABASE_TEST_VIDEO"]
+local_video_path = 'local_video.mp4'
+download_video(video_url, local_video_path)
+
+signature_ref=connect_signature_database()
+
 user_data={
     "name":"Robert Downey Jr.",
     "email":"robertdowney@gmail.com",
 }
-generate_key()
-metadata=extract_metadata(os.environ["SUPABASE_TEST_VIDEO"])
-frames=frame_capture(os.environ["SUPABASE_TEST_VIDEO"])
-sign_combined_data(user_data,metadata,frames)
-with open("signature.bin","rb") as f:
-    signature=f.read()
-verify_signature(user_data,metadata,frames,signature)
+private_pem,public_pem=generate_key()
+metadata=extract_metadata(video_url)
+frames=frame_capture(video_url)
+signature,combined_data=sign_combined_data(user_data,metadata,frames)
+
+data={
+    "private_pem":private_pem,
+    "public_pem":public_pem,
+    "signature":signature,
+    "combined_data":combined_data
+}
+signature_ref.add(data)
+
+set_signature_and_public_key(video_url,signature,public_pem)
+public_key,signature=extract_signature_and_public_key()
+verify_signature(user_data,metadata,frames,signature,public_key)
 
--- backend/firestore_connection/firestore.py
+++ backend/firestore_connection/firestore.py
@@ -1,6 +1,11 @@
 from firebase_admin import credentials, firestore, initialize_app
+import os
+import sys
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(base_dir)
+credential_path = os.path.join(base_dir, 'backend', 'firestore_connection', 'b27project-31bde-firebase-adminsdk-ybhhu-2459f43c40.json')
 
-cred = credentials.Certificate("firestore_connection/b27project-31bde-firebase-adminsdk-ybhhu-2459f43c40.json")
+cred = credentials.Certificate(credential_path)
 default_app = initialize_app(cred)
 db = firestore.client()
 
@@ -13,3 +18,7 @@ def video_url_to_firestore():
   video_ref = db.collection('videos')
   print("dsvjhsvkhdsvf gjfv sagvjdfgvfghv dfjv dgjv gh vgfv ghd vghd ghd ghd hgdz ghzfdhd zdgh zdhg h h hzd hd d   zdh zgh d zhfd ")
   return video_ref
+
+def connect_signature_database():
+  signature_ref=db.collection('signature')
+  return signature_ref
