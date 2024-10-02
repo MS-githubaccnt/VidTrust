7857cb5ca04cf00cd9257bbade3bf8e98149a7ee
Tanisha Dixit <dixittanisha02@gmail.com>
2024-10-03 04:41:48 +0530

    feat: adds add_noise_to_audio and audio_analysis function and add their results to the report

--- backend/controllers/video_controllers.py
+++ backend/controllers/video_controllers.py
@@ -5,11 +5,16 @@ from urllib.parse import urlencode
 import jwt
 import requests
 import uuid
+import time
+from backend.audio_analysis.aud import add_noise_to_audio, extract_audio, plot_audio_waveforms, replace_audio_in_video
+from backend.audio_analysis.audio_to_gemini_api import compare_transcript
+from backend.audio_analysis.convert import stereo_to_mono
+from backend.audio_analysis.gemini_api import gemini_insights
 from google_video_api.gemini_report_generation import generate_report
 from google_video_api.google_api_video import detect_potential_tampering
 from firestore_connection.firestore     import video_url_to_firestore,temp_video_url_to_firestore
 import logging
-from controllers.signature_controllers import convert_mp4_to_mkv, download_video,upload_signed_video,verify_signed_video
+from controllers.signature_controllers import convert_mp4_to_mkv, download_video, update_video_on_supabase,upload_signed_video,verify_signed_video
 from google.cloud import storage
 from datetime import timedelta
 logger = logging.getLogger(__name__)
@@ -51,8 +56,9 @@ def video_url():
     id = str(uuid.uuid4())
     video_ref = video_url_to_firestore()
     video_ref.document(id).set(video_struct)
-    print("ye lo tumhare upload signed video function ke arguments",      user_data,videoUrl)
-    upload_signed_video(user_data,videoUrl['data'])
+    print("ye lo tumhare upload signed video function ke arguments",user_data,videoUrl)
+    add_noise_to_video(videoUrl)
+    upload_signed_video(user_data,videoUrl)
     return jsonify({"status": "success", "message": "Data received and uploaded"}), 200
 
 
@@ -125,14 +131,17 @@ def test_video():
     id = str(uuid.uuid4())
     video_ref = video_url_to_firestore()
     video_ref.document(id).set(video_struct)
-    signature_verification_result=verify_signed_video(videoUrl)
+    signature_verification_result,original_video_url=verify_signed_video(videoUrl)
     signedUrl=upload_to_google_bucket(videoUrl)
     print("ye lo tumhari signedUrl",signedUrl)
+    audio_analysis_report,similarity_percentage=audio_analysis(videoUrl,original_video_url)
     tampering_detection_result=detect_potential_tampering(signedUrl)
-    report=generate_report(tampering_detection_result)
+    video_intelligence_report=generate_report(tampering_detection_result)
     message = {
         'Signature verification result': signature_verification_result,
-        'Tampering detection result': report
+        'Tampering detection result': video_intelligence_report,
+        'Audio analysis result':audio_analysis_report,
+        'Audio similarity percentage':similarity_percentage
     }
     return jsonify({"status": "success", "message": message}), 200
 
@@ -181,4 +190,41 @@ def upload_to_google_bucket(video_url):
     return url
 
 
-    
+def add_noise_to_video(video_url):
+    original_audio="original_audio.wav"
+    noisy_audio="noisy_audio.wav"
+    local_video_path="local_video.mkv"
+    output_video_path="output_video.mkv"
+    remote_file_path=video_url.split("/")[-1]
+    download_video(video_url,local_video_path)
+    extract_audio(local_video_path,original_audio)
+    add_noise_to_audio(original_audio, noisy_audio, 0.5)
+    replace_audio_in_video(local_video_path, noisy_audio, output_video_path)
+    update_video_on_supabase(output_video_path,remote_file_path)
+    time.sleep(10)
+    # os.remove(local_video_path)
+    os.remove(output_video_path)
+    os.remove(original_audio)
+    os.remove(noisy_audio)
+
+def audio_analysis(test_video_url,original_video_url):
+    original_audio="original_audio.wav"
+    original_audio_mono="original_audio_mono.wav"
+    test_audio="test_audio.wav"
+    test_audio_mono="test_audio_mono.wav"
+    graph="plot.png"
+    extract_audio(test_video_url,test_audio)
+    extract_audio(original_video_url,original_audio)
+    plot_audio_waveforms(original_audio,test_audio)
+    report=gemini_insights(graph)
+    stereo_to_mono(original_audio,original_audio_mono)
+    stereo_to_mono(test_audio,test_audio_mono)
+    similarity_percentage=compare_transcript(original_audio_mono,test_audio_mono)
+    os.remove(original_audio)
+    os.remove(test_audio)
+    os.remove(original_audio_mono)
+    os.remove(test_audio_mono)
+    os.remove(graph)
+    return report,similarity_percentage
+
+
