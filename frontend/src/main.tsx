b18c3ea13ef178876179c710e0831d29bbc187bd
Tanisha Dixit <dixittanisha02@gmail.com>
2024-10-02 20:18:25 +0530

    fix: fixes sidebar provider error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AuthContextProvider } from './context/AuthContext';
 import App from './App';
+import SidebarProvider from './context/TabContext';
 
 createRoot(document.getElementById('root')!).render(
     <AuthContextProvider>
-        <App/>
+        <SidebarProvider>
+            <App/>
+        </SidebarProvider>
     </AuthContextProvider>
 )
