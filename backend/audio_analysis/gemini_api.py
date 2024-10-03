39c6f516cb8d6ca47b7634ac9e8147c2b3f9e36a
DeenankSharma <sharmadeenank@gmail.com>
2024-10-03 06:35:34 +0530

    minor changes

--- backend/audio_analysis/gemini_api.py
+++ backend/audio_analysis/gemini_api.py
@@ -1,7 +1,8 @@
 import google.generativeai as genai
+import os
 
 def gemini_insights(graph):
-  genai.configure(api_key="AIzaSyD2xeYk9N9HDXR7suqU0650_QlOzwMO0lc")
+  genai.configure(api_key=os.environ["API_KEY"])
   myfile = genai.upload_file(graph)
   model = genai.GenerativeModel("gemini-1.5-flash")
   response = model.generate_content([myfile, "\n\n", "What are your insights of this image"])
